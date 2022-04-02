c2f4571467cadc0454a3cec0f248ccd2ae200f3e
Jayanth Dungavath <jayanthdeejay@gmail.com>
2022-04-03 00:59:13 +0530

    basics of variables and contants

--- main.go
+++ main.go
@@ -4,6 +4,16 @@ import (
 	"fmt"
 )
 
+// I'm following a youtube tutorial to develop a basic booking application
+// https://www.youtube.com/watch?v=yyUHQIec83I
+
 func main() {
-	fmt.Println("Hello World!")
+	var conferenceName = "Go conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Println("We have totol of", conferenceTickets, "and we have", remainingTickets, "available")
+	fmt.Println("Get your tickets here to attend")
+
 }
