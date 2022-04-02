32ca7d2aa7ef3382c2cb0e0847fedd5c4719f9ba
Jayanth Dungavath <jayanthdeejay@gmail.com>
2022-04-03 01:43:42 +0530

    using arrays to store booking information

--- main.go
+++ main.go
@@ -12,6 +12,11 @@ func main() {
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
 
+	// Following can be used to declare on array
+	// var bookings = [50]string{}
+	// An alternate declaration
+	var bookings [50]string
+
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have totol of %v and we have %v available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
@@ -29,6 +34,11 @@ func main() {
 	fmt.Print("Enter number of tickets: ")
 	fmt.Scan(&userTickets)
 	remainingTickets = remainingTickets - userTickets
+	bookings[0] = firstName + " " + lastName
+
+	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The first booking: %v\n", bookings[0])
+
 	fmt.Printf("Thank you %v %v for booking %v tickets. Tickets will be sent to %v\n", firstName, lastName, userTickets, userEmail)
 	fmt.Printf("%v tickets are remaining for %v\n", remainingTickets, conferenceName)
 }
