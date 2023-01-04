1b30d06e5ef76cebc3fe404df7dfa90fc2480433
Jayanth Dungavath <jayanthdeejay@gmail.com>
2023-01-04 22:10:03 +0000

    refactor repeated code

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -7,16 +7,21 @@ func TestHello(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello, Chris"
 
-		if got != want {
-			t.Errorf("got %q, and want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got %q, and want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+// testing.TB accecpts both testing.T and testing.B
+// testing.TB is an interface
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper() // helps debug errors
+	if got != want {
+		t.Errorf("got %q, and want %q", got, want)
+	}
 }
