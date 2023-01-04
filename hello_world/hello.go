37b5043f9acd10bec3713ab4dae9a8fcf820ae5a
Jayanth Dungavath <jayanthdeejay@gmail.com>
2023-01-04 22:36:45 +0000

    added comments

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 const spanish = "Spanish"
 const french = "French"
@@ -8,21 +10,30 @@ const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
 const frenchHelloPrefix = "Bonjour, "
 
+// public function names start with uppercase
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+}
 
+// private function names start with lowercase
+// (prefix string) in signature will create prefix with "" default value
+// we could use return without having to specify prefix
+// also helps with godoc
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case french:
 		prefix = frenchHelloPrefix
 	case spanish:
 		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-
-	return prefix + name
+	//
+	return
 }
 
 func main() {
